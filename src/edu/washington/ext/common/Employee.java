f6743a9adaf9b3aa5c906d2b339aeb1fd0048984
thisfix <lukedecker1@gmail.com>
2015-03-08 17:35:08 -0700

    initial setup

---
+++ src/edu/washington/ext/common/Employee.java
@@ -0,0 +1,22 @@
+package edu.washington.ext.common;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface Employee.
+ */
+public interface Employee {
+	 
+ 	/**
+ 	 * Gets the name.
+ 	 *
+ 	 * @return the name
+ 	 */
+ 	String getName();
+	 
+ 	/**
+ 	 * Calculate pay.
+ 	 *
+ 	 * @return the double
+ 	 */
+ 	double calculatePay();
+}
